f7546e4520aa190dc017db5cf7c31871b346708b
Idham <idham16iramap@gmai.com>
2022-10-10 02:31:52 +0700

    update ionic university of london

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: './tab3.page.html',
   styleUrls: ['./tab3.page.scss'],
 })
-export class Tab3Page implements OnInit {
+export class Tab3Page{
 
-  constructor() { }
+  constructor(
+    private storage : Storage,
+    private navCtrl : NavController
+  ) { }
 
   ngOnInit() {
   }
-
+  logout(){
+    this.storage.clear();
+    this.navCtrl.navigateRoot('/welcome');
+  }
 }
